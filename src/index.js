ba04e355ae24236fcbba6f0937feaecc027f8caa
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-03-18 12:41:20 +0100

    Add App.js.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dashboard from './routes/dashboard/Dashboard';
-import FavouriteCars from './routes/favouriteCars/FavouriteCars';
-import SearchResults from './routes/searchResults/SearchResults';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter, Route} from 'react-router-dom';
-import SignInForm from "./routes/signInForm/SignInForm";
 
-const login = true;
-
-
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path='/' component={login ? Dashboard : SignInForm}/>
-            <Route path='/favourite' component={FavouriteCars}/>
-            <Route path='/result' component={SearchResults}/>
-        </div>
-
-
-    </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
