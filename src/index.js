75404ed4f2dc1a4443922fc125f2c9db09c80b89
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-03-27 23:37:22 +0200

    Add combineReucers.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './combineReducers/rootReducer';
 
 import './index.css';
 import App from './App';
@@ -11,6 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
+    rootReducer,
     composeEnhancers(applyMiddleware(thunk))
 );
 
